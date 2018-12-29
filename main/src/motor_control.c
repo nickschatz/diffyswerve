8689525633dc95414e1b1258d3b907f4d573d98b
nickschatz <nick@nickschatz.com>
2018-12-29 12:43:03 -0600

    Add direction pin setting

--- main/src/motor_control.c
+++ main/src/motor_control.c
@@ -3,6 +3,7 @@
 #include "driver/mcpwm.h"
 #include "soc/mcpwm_reg.h"
 #include "soc/mcpwm_struct.h"
+#include "driver/gpio.h"
 
 #include "motor_control.h"
 #include <stdlib.h>
@@ -40,6 +41,21 @@ Motor* createMotor(int gpio_pwm, int gpio_dir) {
         output = MCPWM0A;
     }
 
+    // Configure GPIO
+    gpio_config_t io_conf;
+    //disable interrupt
+    io_conf.intr_type = GPIO_PIN_INTR_DISABLE;
+    //set as output mode
+    io_conf.mode = GPIO_MODE_OUTPUT;
+    //bit mask of the pins that you want to set,e.g.GPIO18/19
+    io_conf.pin_bit_mask = 1ULL<<gpio_dir;
+    //disable pull-down mode
+    io_conf.pull_down_en = 0;
+    //disable pull-up mode
+    io_conf.pull_up_en = 0;
+    //configure GPIO with the given settings
+    gpio_config(&io_conf);
+
     // Allocate motor on heap
     Motor* motor = (Motor*) malloc(sizeof(Motor));
     motor->gpio_dir = gpio_dir;
@@ -55,10 +71,12 @@ Motor* createMotor(int gpio_pwm, int gpio_dir) {
  */
 void setMotorSpeed(Motor* motor, float speed) {
     float magnitude = fabs(speed);
-    //int direction = speed > 0;
+    int direction = speed > 0;
     // Clamp magnitude
     magnitude = fmin(fmax(magnitude, 0), 1.0);
     // Interpolate magnitude to us
     int micros = MIN_PULSE_US + (MAX_PULSE_US - MIN_PULSE_US) * magnitude;
     mcpwm_set_duty_in_us(motor->unit, motor->timer, motor->op, micros);
+    // Write direction pin
+    gpio_set_level(motor->gpio_dir, direction);
 }
